a4819a3a5614250f47746223ec54febb54115b61
German Vasilevich <germanvasilevic@gmail.com>
2024-09-11 18:10:31 +0300

    version with gtests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/german/Документы/453504/ОАиП/LR1
+# Build directory: /home/german/Документы/453504/ОАиП/LR1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[add]=] "a.out")
+set_tests_properties([=[add]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/german/Документы/453504/ОАиП/LR1/CMakeLists.txt;10;add_test;/home/german/Документы/453504/ОАиП/LR1/CMakeLists.txt;0;")
