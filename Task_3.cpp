e4ea66fd8907ced880a4c6e1fc71b1b57ed23a8f
German Vasilevich <germanvasilevic@gmail.com>
2024-09-11 16:53:41 +0300

    first-commit

---
+++ Task_3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+#include<cmath>
+
+
+int main() {
+    double touble, start, end, step;
+    std::cout << "Введите начальное значение x: ";
+    std::cin >> start;
+    std::cout << "Введите конечное значение x: ";
+    std::cin >> end;
+    std::cout << "Введите шаг: ";
+    std::cin >> step;
+    for (double x = start; x <= end; x += step) {
+    double y = sin(x);
+    std::cout << x << " | " << y << std::endl;
+    }
+    return 0;
+}
